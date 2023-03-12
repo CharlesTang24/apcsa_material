c8a4b7a94ba934fce72302486d3fba5c64ec59a5
CharlesTang24 <124080330+CharlesTang24@users.noreply.github.com>
2023-03-11 22:13:09 -0500

    Update MyLinkedList.java

--- lessons/linked_list/MyLinkedList.java
+++ lessons/linked_list/MyLinkedList.java
@@ -13,7 +13,11 @@ public class MyLinkedList{
 
   /* Add a new node which data value to the front of the list */
   public void add(String value){
-
+	if(head == null) {
+		head = new Node(null,null);
+	}
+	Node addNode = new Node(value, head.getNext());
+    head.setNext(addNode);
   }
 
   /* Returns the value in the node at location index. */
@@ -23,38 +27,70 @@ public class MyLinkedList{
     Node walker = head;
     // Counter to track a node position
     int counter = 0;
-    
-    return "";
+    while(counter < index) {
+      walker = walker.getNext();
+      counter++;
+    }
+    return walker.getNext().getData();
   }
 
   /* Return the list as a string */
   public String toString(){
     // Create a reference to head. It will help loop through the list
     Node walker = head;
-    
-    return "";
+    String returnString = "";
+    while(walker != null){
+      returnString += walker.getNext();
+      walker = walker.getNext();
+    }
+    return returnString;
   }
 
   /* Returns the number of elements in the list */
   public int size(){
-    return 0;
+    Node walker = head;
+    int NumOfElements = 0;
+    while(walker.getNext() != null){
+      walker = walker.getNext();
+      NumOfElements++;
+    }
+    return NumOfElements;
   }
 
 
   /*Adds a new node which contains value at index */
   public void add(int index, String value){
-
+	  Node walker = head;
+	  for(int counter = 0; counter < index; counter++) {
+		  walker = walker.getNext();
+	  }
+	  Node addNode = new Node(value, walker.getNext());
+	  walker.setNext(addNode);
   }
 
 
   /* Returns the index of the first node in the list that contains value. */
   public int indexOf(String value){
-    return 0;
+	  Node walker = head;
+	  int counter = 0;
+	  while(walker.getNext() != null) {
+		  if(walker.getNext().getData() == value)
+			  return counter;
+		  walker = walker.getNext();
+		  counter++;
+	  }
+      return -1;
   }
 
 
   /* Remove node al location index */
 
   public void remove(int index){
+	  Node walker = head;
+	  for(int counter = 0; counter < index; counter++) {
+		  walker = walker.getNext();
+		  System.out.println(walker);
+	  }
+	  walker.setNext(walker.getNext().getNext());
   }
 }
